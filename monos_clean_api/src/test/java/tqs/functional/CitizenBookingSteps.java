55212e81ed92c0f306a16190607c9a630f9f5a16
MartaDias05 <marta.dias.cruz@icloud.com>
2025-11-02 09:42:59 +0000

    made browser headless for citizen steps test

--- monos_clean_api/src/test/java/tqs/functional/CitizenBookingSteps.java
+++ monos_clean_api/src/test/java/tqs/functional/CitizenBookingSteps.java
@@ -22,7 +22,7 @@ public class CitizenBookingSteps {
     @Before
     public void setUp() {
         Playwright playwright = Playwright.create();
-        browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+        browser = playwright.chromium().launch();
         context = browser.newContext();
         page = context.newPage();
     }
